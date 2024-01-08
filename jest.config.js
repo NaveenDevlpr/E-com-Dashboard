58fbe241fd4662e95220fc1b3219045a8ad8eb93
naveen <naveenmk675@gmail.com>
2024-01-08 16:29:00 +0530

    updated testing dependencies

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest')
+ 
+/** @type {import('jest').Config} */
+const createJestConfig = nextJest({
+  
+  dir: './',
+})
+ 
+
+const config = {
+  coverageProvider: 'v8',
+  testEnvironment: 'jsdom',
+  
+}
+ 
+
+module.exports = createJestConfig(config)
